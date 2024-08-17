5ae58b71b8674ec3d41ed1e963338822228fbc65
Bhavesh Sharma <bhaveshsharma8@gmail.com>
2024-08-18 02:05:17 +0530

    Revert "intialiazing"
    This reverts commit b4cbbc942a777647afc146523b6bccec532ea3b8.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ function App() {
   
   return (
     <div className='h-screen '>
-
+      <div className='h-[25%] bg-gray-200 font-sans font-bold text-6xl text-blue-300 flex justify-center items-center'>
+        REACT DRAG AND DROP <div className='text-black ml-2 text-3xl contents'>  component</div>
+      </div>
       <div className='h-[75%]'>
       <Canvas/>
         </div> 
